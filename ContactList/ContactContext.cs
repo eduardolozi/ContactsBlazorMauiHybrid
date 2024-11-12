5419ee00d71180175d81fa543de4691f6b30bc31
Eduardo Lozano <eduardo.azevedo@waydatasolution.com>
2024-11-12 16:09:08 -0300

    .

---
+++ ContactList/ContactContext.cs
@@ -0,0 +1,12 @@
+﻿using ContactList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactList;
+
+public class ContactContext : DbContext
+{
+    public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+}
---
+++ ContactList/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using ContactList.Models;
+
+namespace ContactList.Services;
+
+public class UserService(ContactContext contactContext) {
+
+    public void Add(User user)
+    {
+        contactContext.Users.Add(user);
+        contactContext.SaveChanges();
+    }
+    
+    public void Edit(User user)
+    {
+        contactContext.Users.Update(user);
+        contactContext.SaveChanges();
+    }
+    
+    public void Delete(User user)
+    {
+        contactContext.Users.Remove(user);
+        contactContext.SaveChanges();
+    }
+    
+    public List<User> GetAll()
+    {
+        return contactContext.Users.ToList();
+    }
+    
+    public User? GetById(int id)
+    {
+        return contactContext.Users.Find(id);
+    }
+}
